ae9443ed395ec3aed116dcfeeb7628496c6677f8
fuvity <gabriel.feitosa@advisia.local>
2019-05-14 20:53:24 -0300

    wtf rodolfo me ajuda

--- graphics.py
+++ graphics.py
@@ -1,6 +1,9 @@
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
+from math import sqrt, ceil
 
 def getChild(A, B):
     if crossover_method == 1:
@@ -14,18 +17,18 @@ def getChild(A, B):
     
 
 # Pegando parametros
-heightmap_size = 300 #int(input('Tamanho do mapa:'))
+heightmap_size = 2000 #int(input('Tamanho do mapa:'))
 max_iterations = 1000 #int(input('Numero maximo de iterações:))
 pop_size = 30#int(input('Tamanho da População: '))
-fittest_selection = 40#int(input('Quantos se reproduzirão por geração: '))
+fittest_selection = 20#int(input('Quantos se reproduzirão por geração: '))
 fittest_selection = int(pop_size*fittest_selection/100)
-mutation_chance = 10#int(input('Chance de Mutação(%): '))
+mutation_chance = 3#int(input('Chance de Mutação(%): '))
 crossover_por_geracao = 50#int(input('Quantos crossover por geracao(%): '))
 crossover_por_geracao = int(pop_size*crossover_por_geracao/100)
-selection_method = 1 #1 = escolhe os maiores. 2 = probabilistico
+selection_method =  1#1 = escolhe os maiores. 2 = probabilistico
 crossover_method = 1 #1 = escolhe media entre pontos, 2 = pega uma coordenada de cada
 mutation_method = 1 #1 = troca coordenadas, 2 = divide coordenadas por 2
-choice_stop = 1 #1 = max iterations, 2 = std dev. tem de ser 15% do inicial
+choice_stop = 1 #1 = max iterations, 2 = std dev. tem de ser 1/30 do inicial
 
 # Inventando um heightmap
 X = np.arange(0, heightmap_size, 1)
@@ -54,7 +57,6 @@ for _ in range(pop_size):
     childreny.append(y)
     childrenz.append(z)
     children.append((x, y, z))
-
 ax = plt.figure()
 plt.title('Primeira geração')
 p = plt.imshow(Z, cmap=cm.viridis)
@@ -63,27 +65,28 @@ marker_size = 10
 plt.scatter(childrenx, childreny, marker_size, c='r')
 plt.show()
 
+itt = 0 
 if choice_stop == 1:
-    itt = 0 
     condition = 'itt < max_iterations'
 else:
     deviation = np.std(childrenz)
-    condition = 'deviation >' + str(deviation/10)
-    print(deviation)
-
+    deviationtarget = np.std(childrenz)/30
+    condition = 'deviation >' + str(deviationtarget)
+    
 while eval(condition):
     # Survival of the fittest
     if selection_method == 1:
         children = sorted(children, key=lambda x: x[2])
-        fittest = children[fittest_selection:]
+        fittest = children[(pop_size-fittest_selection):]
     else:
         keys = []
         [keys.append(x[2]) for x in children]
         total = sum(keys)
         for i in range(len(keys)):
             keys[i] = keys[i]/total
+        fittest = []
         idx = np.random.choice(pop_size, fittest_selection, keys)
-        fittest = np.array(children)[idx]
+        fittest = list(np.array(children)[idx])
         fittest = sorted(fittest, key=lambda x: x[2])
     childrencopy = children
     History.append(fittest[-1])
@@ -92,19 +95,17 @@ while eval(condition):
     children = []
     [children.append(fitboy) for fitboy in fittest] #fittest vai direto pra prox geracao
     for i in range(crossover_por_geracao): # quem faz crossover mistura os pais
-        children.append(getChild(fittest[(-i)%fittest_selection], fittest[(-i-1)%fittest_selection]))
+        children.append(getChild(fittest[np.random.randint(len(fittest))], fittest[np.random.randint(len(fittest))]))
     for i in range(len(children)): # mutacao
         if np.random.randint(100) < mutation_chance:
             child = children.pop(i)
             if mutation_method == 1:
                 x = int(child[1])
                 y = int(child[0])
-                children.append((x, y, Z[x][y]))
             else:
-                x = child[0]//2
-                y = child[1]//2
-                children.append((x, y, Z[x][y]))
-                
+                x = np.random.randint(heightmap_size)
+                y = np.random.randint(heightmap_size)
+            children.append((x,y,Z[x][y]))
     for i in range(pop_size-len(children)): # completa ate pop_size
         children.append(childrencopy[i])
 
@@ -135,7 +136,7 @@ while eval(condition):
         marker_size = 10
         plt.scatter(childrenx, childreny, marker_size, c='r')
         plt.show()
-
+    
     if itt == max_iterations-1:
         ax = plt.figure()
         plt.title('Última geração')
@@ -149,16 +150,14 @@ while eval(condition):
         marker_size = 10
         plt.scatter(childrenx, childreny, marker_size, c='r')
         plt.show()
-    
-    if choice_stop == 1:
-        itt += 1
-    else:
+        
+    itt += 1
+    if choice_stop == 2:
         childrenz = []
         for child in children:
             childrenz.append(child[2])
         deviation = np.std(childrenz)
-# Fazendo as figuras
-
+        
 childrenx = []
 childreny = []
 contador = []
